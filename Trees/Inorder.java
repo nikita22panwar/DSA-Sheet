2aaf5792c97dc64779e4a22023e7576acb3de03b
Nikita Panwar <78353504+nikita22panwar@users.noreply.github.com>
2023-04-24 22:41:40 +0530

    Create Inorder.java

---
+++ Trees/Inorder.java
@@ -0,0 +1,13 @@
+public class Solution {
+    ArrayList<Integer>res=new ArrayList<Integer>();
+    public List<Integer> inorderTraversal(TreeNode root) {
+       
+       if(root==null){
+           return res;
+       }
+       inorderTraversal(root.left);
+       res.add(root.val);
+       inorderTraversal(root.right);
+        return res;
+    }
+}
